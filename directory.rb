7470cb18f5ec5447816e9dad999ce8a00fbaabfc
Francesco <francescocollotto@gmail.com>
2021-11-06 12:08:31 +0000

    add input_students method

--- directory.rb
+++ directory.rb
@@ -1,17 +1,23 @@
 #let's put all the students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "to finish just hit return twice"
+  # create empty students array
+  students = []
+  # get the input name 
+  name = gets.chomp
+  # while the name is not empty repeat this code
+  while !name.empty? do
+    # add the student name and cohort to students list
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # ask for user input again to set name
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
 #then we print them
 def print_header
   puts "The students of Villains Academy"
@@ -29,6 +35,7 @@ def print_footer(students)
 end
 
 #nothing happens until we call the methods
+students = input_students
 print_header
 print_names(students)
 print_footer(students)
