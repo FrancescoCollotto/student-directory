12a63b9fc9d9e7a5af9344d8cb19fa22908453f1
Francesco <francescocollotto@gmail.com>
2021-11-07 12:53:18 +0000

    refactor directory.rb

--- directory.rb
+++ directory.rb
@@ -1,21 +1,18 @@
-#let's put all the students into an array
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "to finish just hit return twice"
-  # create empty students array
-  students = []
   # get the input name 
   name = gets.chomp
   # while the name is not empty repeat this code
   while !name.empty? do
     # add the student name and cohort to students list
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} #{students.count == 1 ? "student" : "students"}"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} #{@students.count == 1 ? "student" : "students"}"
     # ask for user input again to set name
     name = gets.chomp
   end
-  # return the array of students
-  students
 end
 
 #then we print them
@@ -42,29 +39,35 @@ def print_footer(students)
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. exit"
+end
+
+def show_students
+  print_header
+  print_names(@students)
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't understand what you are saying, try again"
+  end
+end
+
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. exit"
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print_names(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't understand what you are saying, try again"
-    end
-    # 4. repeat from step 1
+    print_menu
+    process(gets.chomp)
   end
 end
 
